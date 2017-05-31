f81b5bfbbd5dc1c9db26aaa464de6d18c3feb22e
Ira <iramonko97@gmail.com>
2017-06-01 00:48:29 +0300

    Update task 4.py

--- task 4.py
+++ task 4.py
@@ -1,10 +1,13 @@
 import sqlite3
 
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
 cur.execute('''
 DROP TABLE IF EXISTS Counts''')
 
 cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (email TEXT, count INTEGER , mount Text)''')
 
 fname = raw_input('Enter file name: ')
 if (len(fname) < 1): fname = 'mbox.txt'
@@ -14,8 +17,8 @@ for line in fh:
     pieces = line.split()
     email = pieces[1]
     mount = pieces[3]
-    
-  cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (email,mount, count)
